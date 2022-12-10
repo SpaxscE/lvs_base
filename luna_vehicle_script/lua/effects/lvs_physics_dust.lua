62d68a47e2a7d5ac17920dd9bfb6b14b523d4e4c
Luna <swagnemite@gmx.de>
2022-12-10 02:10:34 +0100

    split wind fx => water / dust

--- luna_vehicle_script/lua/effects/lvs_physics_dust.lua
+++ luna_vehicle_script/lua/effects/lvs_physics_dust.lua
@@ -23,67 +23,16 @@ local MatDebris = {
 }
 
 function EFFECT:Init( data )
+	local Pos = data:GetOrigin()
 	local Ent = data:GetEntity()
 
 	if not IsValid( Ent ) then return end
 
-	local CenterPos = Ent:LocalToWorld( Ent:OBBCenter() )
-
-	local trace = util.TraceLine( {
-		start = CenterPos + Vector(0,0,25),
-		endpos = CenterPos - Vector(0,0,300),
-		filter = Ent,
-	} )
-
-	local traceWater = util.TraceLine( {
-		start = CenterPos + Vector(0,0,25),
-		endpos = CenterPos - Vector(0,0,300),
-		filter = Ent,
-		mask = MASK_WATER,
-	} )
-
-	if traceWater.Hit and trace.HitPos.z < traceWater.HitPos.z then 
-		self.LifeTime = math.Rand(1.5,3)
-		self.DieTime = CurTime() + self.LifeTime
-
-		self.Splash = {
-			Pos = traceWater.HitPos,
-			Mat = Material("effects/splashwake1"),
-			RandomAng = math.random(0,360),
-		}
-
-		local Pos = traceWater.HitPos
-
-		local emitter = Ent:GetParticleEmitter()
-
-		if emitter and emitter.Add then
-			local particle = emitter:Add( "effects/splash4", Pos + VectorRand(-10,10) - Vector(0,0,20) )
-			if particle then
-				particle:SetVelocity( Vector(0,0,250) )
-				particle:SetDieTime( 0.8 )
-				particle:SetAirResistance( 60 ) 
-				particle:SetStartAlpha( 255 )
-				particle:SetEndAlpha( 0 )
-				particle:SetStartSize( 50 )
-				particle:SetEndSize( 100 )
-				particle:SetRoll( math.Rand(-1,1) * 100 )
-				particle:SetColor( 255,255,255 )
-				particle:SetGravity( Vector( 0, 0, -600 ) )
-				particle:SetCollide( false )
-			end
-		end
-
-		return
-	end
-
 	self.DieTime = CurTime()
 
-	if not trace.Hit then return end
-
-	local Pos = trace.HitPos
 	local Dir = Ent:GetForward()
 
-	local emitter = Ent:GetParticleEmitter()
+	local emitter = Ent:GetParticleEmitter( Ent:GetPos() )
 
 	local VecCol = render.GetLightColor( Pos + Vector(0,0,10) ) * 0.5 + Vector(0.3,0.25,0.15)
 
@@ -135,15 +84,4 @@ function EFFECT:Think()
 end
 
 function EFFECT:Render()
-	if self.Splash and self.LifeTime then
-		local Scale = (self.DieTime - self.LifeTime - CurTime()) / self.LifeTime
-		local S = 200 - Scale * 600
-		local Alpha = 100 + 100 * Scale
-
-		cam.Start3D2D( self.Splash.Pos + Vector(0,0,1), Angle(0,0,0), 1 )
-			surface.SetMaterial( self.Splash.Mat )
-			surface.SetDrawColor( 255, 255, 255 , Alpha )
-			surface.DrawTexturedRectRotated( 0, 0, S , S, self.Splash.RandomAng )
-		cam.End3D2D()
-	end
 end
---
+++ luna_vehicle_script/lua/effects/lvs_physics_water.lua
@@ -0,0 +1,57 @@
+
+function EFFECT:Init( data )
+	local Pos = data:GetOrigin()
+	local Ent = data:GetEntity()
+
+	if not IsValid( Ent ) then return end
+
+	self.LifeTime = math.Rand(1.5,3)
+	self.DieTime = CurTime() + self.LifeTime
+
+	self.Splash = {
+		Pos = Pos,
+		Mat = Material("effects/splashwake1"),
+		RandomAng = math.random(0,360),
+	}
+
+	local emitter = Ent:GetParticleEmitter( Ent:GetPos() )
+
+	if emitter and emitter.Add then
+		local particle = emitter:Add( "effects/splash4", Pos + VectorRand(-10,10) - Vector(0,0,20) )
+		if particle then
+			particle:SetVelocity( Vector(0,0,250) )
+			particle:SetDieTime( 0.8 )
+			particle:SetAirResistance( 60 ) 
+			particle:SetStartAlpha( 255 )
+			particle:SetEndAlpha( 0 )
+			particle:SetStartSize( 50 )
+			particle:SetEndSize( 100 )
+			particle:SetRoll( math.Rand(-1,1) * 100 )
+			particle:SetColor( 255,255,255 )
+			particle:SetGravity( Vector( 0, 0, -600 ) )
+			particle:SetCollide( false )
+		end
+	end
+end
+
+
+function EFFECT:Think()
+	if CurTime() > self.DieTime then
+		return false
+	end
+	return true
+end
+
+function EFFECT:Render()
+	if self.Splash and self.LifeTime then
+		local Scale = (self.DieTime - self.LifeTime - CurTime()) / self.LifeTime
+		local S = 200 - Scale * 600
+		local Alpha = 100 + 100 * Scale
+
+		cam.Start3D2D( self.Splash.Pos + Vector(0,0,1), Angle(0,0,0), 1 )
+			surface.SetMaterial( self.Splash.Mat )
+			surface.SetDrawColor( 255, 255, 255 , Alpha )
+			surface.DrawTexturedRectRotated( 0, 0, S , S, self.Splash.RandomAng )
+		cam.End3D2D()
+	end
+end
