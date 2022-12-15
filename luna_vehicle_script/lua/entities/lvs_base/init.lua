a25dba6fe48c372c4001caf92df7e4216fe274e1
Luna <swagnemite@gmx.de>
2022-12-15 18:34:52 +0100

    TeamPassenger convar now functional

--- luna_vehicle_script/lua/entities/lvs_base/init.lua
+++ luna_vehicle_script/lua/entities/lvs_base/init.lua
@@ -109,7 +109,7 @@ end
 function ENT:Use( ply )
 	if not IsValid( ply ) then return end
 
-	if self:GetlvsLockedStatus() then 
+	if self:GetlvsLockedStatus() or (LVS.TeamPassenger:GetBool() and ((self:GetAITEAM() ~= ply:lvsGetAITeam()) and ply:lvsGetAITeam() ~= 0 and self:GetAITEAM() ~= 0)) then 
 
 		self:EmitSound( "doors/default_locked.wav" )
 
