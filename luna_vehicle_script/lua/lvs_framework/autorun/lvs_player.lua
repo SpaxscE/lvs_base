39bcc5347ec584436ddb956423c4970411cb5f5e
Luna <swagnemite@gmx.de>
2022-12-23 23:08:03 +0100

    fix key input bug

--- luna_vehicle_script/lua/lvs_framework/autorun/lvs_player.lua
+++ luna_vehicle_script/lua/lvs_framework/autorun/lvs_player.lua
@@ -144,7 +144,7 @@ function meta:lvsSetInputDisabled( disable )
 	if disable then
 		self._lvsKeyDisabler = CurTime() + 60
 	else
-		self._lvsKeyDisabler = CurTime() + 0.1
+		self._lvsKeyDisabler = CurTime() + 1
 	end
 end
 
