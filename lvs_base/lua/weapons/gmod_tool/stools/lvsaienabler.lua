0c933637728c7d69e361d1a31ba48aac9a977f56
Luna <swagnemite@gmx.de>
2023-01-01 12:39:30 +0100

    ai enabler tool from lfs

---
+++ lvs_base/lua/weapons/gmod_tool/stools/lvsaienabler.lua
@@ -0,0 +1,40 @@
+
+TOOL.Category		= "LVS"
+TOOL.Name			= "#AI Enabler"
+TOOL.Command		= nil
+TOOL.ConfigName		= ""
+
+if CLIENT then
+	language.Add( "tool.lvsaienabler.name", "AI Enabler" )
+	language.Add( "tool.lvsaienabler.desc", "A tool used to enable/disable AI on LVS-Vehicles" )
+	language.Add( "tool.lvsaienabler.0", "Left click on a LVS-Vehicle to enable AI, Right click to disable." )
+	language.Add( "tool.lvsaienabler.1", "Left click on a LVS-Vehicle to enable AI, Right click to disable." )
+end
+
+function TOOL:LeftClick( trace )
+	local ent = trace.Entity
+
+	if not IsValid( ent ) or not ent.LVS then return false end
+
+	if isfunction( ent.SetAI ) then
+		ent:SetAI( true )
+	end
+
+	return true
+end
+
+function TOOL:RightClick( trace )
+	local ent = trace.Entity
+
+	if not IsValid( ent ) or not ent.LVS then return false end
+
+	if isfunction( ent.SetAI ) then
+		ent:SetAI( false )
+	end
+
+	return true
+end
+
+function TOOL:Reload( trace )
+	return false
+end
