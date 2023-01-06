efb9815904d261cb74643ced6c94e7a89a8d1e88
Luna <swagnemite@gmx.de>
2023-01-06 01:56:04 +0100

    moved info text into plane script

--- lvs_base/lua/entities/lvs_base/cl_hud.lua
+++ lvs_base/lua/entities/lvs_base/cl_hud.lua
@@ -6,55 +6,7 @@ LVS:AddHudEditor( "VehicleInfo", ScrW() - 460, ScrH() - 85,  220, 75, 220, 75, "
 	end
 )
 
-ENT.IconEngine = Material( "lvs/engine.png" )
-
-local Circles = {
-	[1] = {r = -1, col = Color(0,0,0,200)},
-	[2] = {r = 0, col = Color(255,255,255,200)},
-	[3] = {r = 1, col = Color(255,255,255,255)},
-	[4] = {r = 2, col = Color(255,255,255,200)},
-	[5] = {r = 3, col = Color(0,0,0,200)},
-}
-
-local function DrawCircle( X, Y, target_radius, value )
-	local endang = 360 * value
-
-	if endang == 0 then return end
-
-	for i = 1, #Circles do
-		local data = Circles[ i ]
-		local radius = target_radius + data.r
-		local segmentdist = endang / ( math.pi * radius / 2 )
-
-		for a = 0, endang, segmentdist do
-			surface.SetDrawColor( data.col )
-
-			surface.DrawLine( X - math.sin( math.rad( a ) ) * radius, Y + math.cos( math.rad( a ) ) * radius, X - math.sin( math.rad( a + segmentdist ) ) * radius, Y + math.cos( math.rad( a + segmentdist ) ) * radius )
-		end
-	end
-end
-
 function ENT:LVSHudPaintInfoText( X, Y, W, H, ScrX, ScrY, ply )
-	local Throttle = self:GetThrottle()
-	local Col = Throttle <= 1 and color_white or Color(0,0,0,255)
-	local hX = X + W - H * 0.5
-	local hY = Y + H * 0.25 + H * 0.25
-
-	surface.SetMaterial( self.IconEngine )
-	surface.SetDrawColor( 0, 0, 0, 200 )
-	surface.DrawTexturedRectRotated( hX + 4, hY + 1, H * 0.5, H * 0.5, 0 )
-	surface.SetDrawColor( color_white )
-	surface.DrawTexturedRectRotated( hX + 2, hY - 1, H * 0.5, H * 0.5, 0 )
-
-	DrawCircle( hX, hY, H * 0.35, math.min( Throttle, 1 ) )
-
-	if Throttle > 1 then
-		draw.SimpleText( "+"..math.Round((Throttle - 1) * 100,0).."%" , "LVS_FONT",  hX, hY, Col, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
-	end
-
-	local kmh = math.Round(self:GetVelocity():Length() * 0.09144,0)
-	draw.DrawText( "km/h ", "LVS_FONT", X + 72, Y + 35, color_white, TEXT_ALIGN_RIGHT )
-	draw.DrawText( kmh, "LVS_FONT_HUD_LARGE", X + 72, Y + 20, color_white, TEXT_ALIGN_LEFT )
 end
 
 function ENT:LVSHudPaintVehicleIdentifier( X, Y, In_Col, target_ent )
