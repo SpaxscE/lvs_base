a1bce23f46eca6962f547058443c6554210af1d7
Luna <swagnemite@gmx.de>
2023-01-08 00:09:38 +0100

    add proton torpedo

---
+++ lvs_starwars/lua/entities/lvs_protontorpedo.lua
@@ -0,0 +1,45 @@
+AddCSLuaFile()
+
+ENT.Base = "lvs_missile"
+
+ENT.Type            = "anim"
+
+ENT.PrintName = "Proton Torpedo"
+ENT.Author = "Luna"
+ENT.Information = "Proton Torpedo - 'geht ab wie'n zäpfchen'"
+ENT.Category = "[LVS]"
+
+ENT.Spawnable		= true
+ENT.AdminOnly		= true
+
+ENT.GlowColor = Color( 0, 127, 255, 255 )
+
+if SERVER then return end
+
+ENT.GlowMat = Material( "sprites/light_glow02_add" )
+
+function ENT:Initialize()	
+	self.snd = CreateSound(self, "npc/combine_gunship/gunship_crashing1.wav")
+	self.snd:SetSoundLevel( 80 )
+	self.snd:Play()
+
+	local effectdata = EffectData()
+		effectdata:SetOrigin( self:GetPos() )
+		effectdata:SetEntity( self )
+	util.Effect( "lvs_proton_trail", effectdata )
+end
+
+function ENT:Draw()
+	self:DrawModel()
+
+	render.SetMaterial( self.GlowMat )
+
+	local pos = self:GetPos()
+	local dir = self:GetForward()
+
+	for i = 0, 30 do
+		local Size = ((30 - i) / 30) ^ 2 * 128
+
+		render.DrawSprite( pos - dir * i * 7, Size, Size, self.GlowColor )
+	end
+end
