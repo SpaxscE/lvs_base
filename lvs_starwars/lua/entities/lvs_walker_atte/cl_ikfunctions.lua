faeb3508ab39a0dae46bfe848153b1821a4f6573
Luna <swagnemite@gmx.de>
2023-01-15 15:58:10 +0100

    added debugoverlay for atte IK script

--- lvs_starwars/lua/entities/lvs_walker_atte/cl_ikfunctions.lua
+++ lvs_starwars/lua/entities/lvs_walker_atte/cl_ikfunctions.lua
@@ -3,6 +3,10 @@ function ENT:OnRemoved()
 	self:LegClearAll()
 end
 
+local debugred = Color(255,0,0,255)
+local debugblue = Color(0,0,255,255)
+local debuggreen = Color(0,255,0,255)
+
 function ENT:LegClearAll()
 	if istable( self.IK_Joints ) then 
 		for _, tab in pairs( self.IK_Joints ) do
@@ -35,14 +39,14 @@ function ENT:GetLegEnts( index, L1, L2, JOINTANG, STARTPOS, ENDPOS, ATTACHMENTS 
 
 	if not self.IK_Joints[ index ] then
 		self.IK_Joints[ index ] = {}
-		
+
 		local BaseProp = ents.CreateClientProp()
 		BaseProp:SetPos( STARTPOS )
 		BaseProp:SetAngles( JOINTANG )
 		BaseProp:SetModel( "models/Combine_Helicopter/helicopter_bomb01.mdl" )
 		BaseProp:SetParent( self )
 		BaseProp:Spawn()
-		
+
 		local LegRotCalc = ents.CreateClientProp()
 		LegRotCalc:SetPos( STARTPOS )
 		LegRotCalc:SetAngles( JOINTANG )
@@ -86,12 +90,12 @@ function ENT:GetLegEnts( index, L1, L2, JOINTANG, STARTPOS, ENDPOS, ATTACHMENTS 
 		self.IK_Joints[ index ].LegEnt2 = prop2
 		self.IK_Joints[ index ].LegEnt3 = prop3
 		self.IK_Joints[ index ].LegEnt4 = prop4
-		
+
 		for _, v in pairs( self.IK_Joints[ index ] ) do
 			v:SetColor( Color( 0, 0, 0, 0 ) )
 			v:SetRenderMode( RENDERMODE_TRANSALPHA )
 		end
-		
+
 		if ATTACHMENTS then
 			if ATTACHMENTS.Leg1 then
 				local prop = ents.CreateClientProp()
@@ -129,15 +133,28 @@ function ENT:GetLegEnts( index, L1, L2, JOINTANG, STARTPOS, ENDPOS, ATTACHMENTS 
 
 	self.IK_Joints[ index ].LegRotCalc:SetAngles(self.IK_Joints[ index ].LegBaseRot:LocalToWorldAngles( self.IK_Joints[ index ].LegBaseRot:WorldToLocal( ENDPOS ):Angle() ) )
 
-	local Dist = math.min( (self.IK_Joints[ index ].LegRotCalc:GetPos() - ENDPOS ):Length(), L1 + L2)
+	local LegRotCalcPos = self.IK_Joints[ index ].LegRotCalc:GetPos()
+
+	local Dist = math.min( (LegRotCalcPos - ENDPOS ):Length(), L1 + L2)
 	local Angle1 = 90 - math.deg( math.acos( (Dist ^ 2 + L1 ^ 2 - L2 ^ 2) / (2 * Dist * L1) ) )
 	local Angle2 = math.deg( math.acos( (Dist ^ 2 + L2 ^ 2 - L1 ^ 2) / (2 * Dist * L2) ) ) + 90
 
 	self.IK_Joints[ index ].LegEnt3:SetAngles( self.IK_Joints[ index ].LegRotCalc:LocalToWorldAngles( Angle(Angle1,180,180) ) )
 	self.IK_Joints[ index ].LegEnt4:SetAngles( self.IK_Joints[ index ].LegRotCalc:LocalToWorldAngles( Angle(Angle2,180,180) ) )
-	
+
 	if self.IK_Joints[ index ].Attachment3 then
 		self.IK_Joints[ index ].Attachment3:SetAngles( self:LocalToWorldAngles( ATTACHMENTS.Foot.Ang ) )
 		self.IK_Joints[ index ].Attachment3:SetPos(self.IK_Joints[ index ].LegEnt2:GetPos() + self:GetForward() * ATTACHMENTS.Foot.Pos.x  + self:GetRight() * ATTACHMENTS.Foot.Pos.y + self:GetUp() * ATTACHMENTS.Foot.Pos.z )
 	end
+
+
+	--debug code
+	local RFT = RealFrameTime()
+	debugoverlay.Cross( self.IK_Joints[ index ].LegBaseRot:GetPos(), 15, RFT, debugred, true )
+	debugoverlay.Line( self.IK_Joints[ index ].LegRotCalc:GetPos(), self.IK_Joints[ index ].LegEnt4:GetPos(), RFT, debuggreen, true )
+	debugoverlay.Cross( self.IK_Joints[ index ].LegEnt4:GetPos(), 15, RFT, debugred, true )
+	debugoverlay.Line( self.IK_Joints[ index ].LegEnt4:GetPos(), self.IK_Joints[ index ].LegEnt2:GetPos(), RFT, debuggreen, true )
+	debugoverlay.Cross( self.IK_Joints[ index ].LegEnt2:GetPos(), 15, RFT, debugred, true )
+	debugoverlay.Cross( self.IK_Joints[ index ].LegEnt1:GetPos(), 4, RFT, debugblue, true )
+	debugoverlay.Sphere( self.IK_Joints[ index ].LegEnt3:GetPos(), 10, RFT, debugblue, true )
 end
