8ecfb737a74e14a8a7ebbf46c675bc445512fb74
Luna <swagnemite@gmx.de>
2023-01-17 22:31:52 +0100

    make laser impact look nicer on brushes

--- lvs_starwars/lua/effects/lvs_laser_impact.lua
+++ lvs_starwars/lua/effects/lvs_laser_impact.lua
@@ -1,22 +1,30 @@
+EFFECT.GlowMat = Material( "sprites/light_glow02_add" )
+
 function EFFECT:Init( data )
 	self.Pos = data:GetOrigin()
+	self.Dir = data:GetNormal()
 	self.Col = data:GetStart() or Vector(255,100,0)
-
-	self.mat = Material( "sprites/light_glow02_add" )
 
 	self.LifeTime = 0.2
 	self.DieTime = CurTime() + self.LifeTime
 
+	local trace = util.TraceLine( {
+		start = self.Pos - self.Dir,
+		endpos = self.Pos + self.Dir,
+		mask = MASK_SOLID_BRUSHONLY,
+	} )
+
+	self.Flat = trace.Hit and not trace.HitSky
+
 	local Col = self.Col
 	local Pos = self.Pos
-	local Dir = data:GetNormal()
 
 	local emitter = ParticleEmitter( Pos, false )
 
 	for i = 0, 10 do
 		local particle = emitter:Add( "sprites/light_glow02_add", Pos )
 
-		local vel = VectorRand() * 200 - Dir  * 80
+		local vel = VectorRand() * 200 + self.Dir  * 80
 
 		if not particle then continue end
 
@@ -49,7 +57,22 @@ end
 
 function EFFECT:Render()
 	local Scale = (self.DieTime - CurTime()) / self.LifeTime
-	render.SetMaterial( self.mat )
-	render.DrawSprite( self.Pos, 200 * Scale, 200 * Scale, Color( self.Col.x, self.Col.y, self.Col.z, 255) )
-	render.DrawSprite( self.Pos, 50 * Scale, 50 * Scale, Color( 255, 255, 255, 255) )
+
+	local S1 = 200 * Scale
+	local S2 = 50 * Scale
+
+	if self.Flat then
+		cam.Start3D2D( self.Pos + self.Dir, self.Dir:Angle() + Angle(90,0,0), 1 )
+			surface.SetMaterial( self.GlowMat )
+			surface.SetDrawColor( self.Col.x, self.Col.y, self.Col.z, 255 )
+			surface.DrawTexturedRectRotated( 0, 0, S1 , S1 , 0 )
+
+			surface.SetDrawColor( 255, 255, 255, 255 )
+			surface.DrawTexturedRectRotated( 0, 0, S2 , S2 , 0 )
+		cam.End3D2D()
+	end
+
+	render.SetMaterial( self.GlowMat )
+	render.DrawSprite( self.Pos + self.Dir, S1, S1, Color( self.Col.x, self.Col.y, self.Col.z, 255 ) )
+	render.DrawSprite( self.Pos + self.Dir, S2, S2, Color( 255, 255, 255, 255) )
 end
