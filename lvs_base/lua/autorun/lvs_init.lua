73547daad854cebfde00ab8e29d1be5990595845
Luna <swagnemite@gmx.de>
2023-01-21 12:15:58 +0100

    smarter networking

--- lvs_base/lua/autorun/lvs_init.lua
+++ lvs_base/lua/autorun/lvs_init.lua
@@ -1,7 +1,7 @@
 
 LVS = istable( LVS ) and LVS or {}
 
-LVS.VERSION = 61
+LVS.VERSION = 62
 LVS.VERSION_GITHUB = 0
 LVS.VERSION_TYPE = ".GIT"
 
--- lvs_base/lua/lvs_framework/autorun/lvs_force_directinput.lua
+++ lvs_base/lua/lvs_framework/autorun/lvs_force_directinput.lua
@@ -1,6 +1,10 @@
 
 local cVar_forcedirect = CreateConVar( "lvs_force_directinput", "0", {FCVAR_REPLICATED , FCVAR_ARCHIVE},"Force Direct Input Steering Method?" )
 
+function LVS:IsDirectInputForced()
+	return LVS.ForceDirectInput == true
+end
+
 if SERVER then
 	util.AddNetworkString( "lvs_forced_input_getter" )
 
@@ -13,10 +17,6 @@ if SERVER then
 		net.Broadcast()
 	end)
 
-	function LVS:IsDirectInputForced()
-		return LVS.ForceDirectInput == true
-	end
-
 	net.Receive( "lvs_forced_input_getter", function( length, ply )
 		net.Start( "lvs_forced_input_getter" )
 			net.WriteBool( LVS:IsDirectInputForced() )
@@ -26,15 +26,9 @@ else
 	net.Receive( "lvs_forced_input_getter", function( length )
 		LVS.ForceDirectInput = net.ReadBool()
 	end )
-	
-	function LVS:IsDirectInputForced()
-		if not isbool( LVS.ForceDirectInput ) then
-			LVS.ForceDirectInput = false
 
-			net.Start( "lvs_forced_input_getter" )
-			net.SendToServer()
-		end
-
-		return LVS.ForceDirectInput
-	end
+	hook.Add( "InitPostEntity", "!11!!!lvsIsPlayerReady", function()
+		net.Start( "lvs_forced_input_getter" )
+		net.SendToServer()
+	end )
 end
