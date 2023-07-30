2cea2e7d7409d5275f7401c1c04c3bc10ac4a7b6
Luna <swagnemite@gmx.de>
2023-07-30 09:35:08 +0200

    remove physgun pickup hook, move CanProperty hook to shared

--- lua/lvs_framework/autorun/lvs_hookers.lua
+++ lua/lvs_framework/autorun/lvs_hookers.lua
@@ -49,10 +49,6 @@ hook.Add( "VehicleMove", "!!!!lvs_vehiclemove", function( ply, vehicle, mv )
 	return true
 end )
 
-hook.Add( "PhysgunPickup", "!!!!lvs_disable_wheel_grab", function( ply, ent )
-	if ent.lvsDoNotGrab then return false end
-end )
-
 hook.Add("CalcMainActivity", "!!!lvs_playeranimations", function(ply)
 	if not ply.lvsGetVehicle then return end
 
@@ -87,13 +83,11 @@ hook.Add( "StartCommand", "!!!!LVS_grab_command", function( ply, cmd )
 	veh:StartCommand( ply, cmd )
 end )
 
-if CLIENT then
-	hook.Add( "CanProperty", "!!!!lvsEditPropertiesDisabler", function( ply, property, ent )
-		if ent.LVS and not ply:IsAdmin() and property == "editentity" then return false end
-	end )
+hook.Add( "CanProperty", "!!!!lvsEditPropertiesDisabler", function( ply, property, ent )
+	if ent.LVS and not ply:IsAdmin() and property == "editentity" then return false end
+end )
 
-	return
-end
+if CLIENT then return end
 
 hook.Add( "EntityTakeDamage", "!!!_lvs_fix_vehicle_explosion_damage", function( target, dmginfo )
 	if not target:IsPlayer() then return end
