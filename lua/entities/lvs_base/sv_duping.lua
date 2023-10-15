fa47ad78b3def5bfeb1434e343f69f4d9e6339b3
Luna <swagnemite@gmx.de>
2023-10-16 00:41:19 +0200

    fix trace filter duping issue

--- lua/entities/lvs_base/sv_duping.lua
+++ lua/entities/lvs_base/sv_duping.lua
@@ -2,6 +2,8 @@
 -- attempt at fixing dupe support
 
 function ENT:PostEntityPaste(Player,Ent,CreatedEntities)
+	if Ent.SetlvsReady then Ent:SetlvsReady( false ) end
+
 	-- allow rebuild of passenger seats
 	Ent.pPodKeyIndex = nil
 	Ent.pSeats = nil
