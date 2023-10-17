569d3f28397dfa443ce4e17b04de0fd89915dedb
Luna <swagnemite@gmx.de>
2023-10-17 16:18:19 +0200

    turn off vehicle on duplicating

--- lua/entities/lvs_base/sv_duping.lua
+++ lua/entities/lvs_base/sv_duping.lua
@@ -3,6 +3,9 @@
 
 function ENT:PostEntityPaste(Player,Ent,CreatedEntities)
 	if Ent.SetlvsReady then Ent:SetlvsReady( false ) end
+	if Ent.GetActive then Ent:SetActive( false ) end
+	if Ent.GetEngineActive then Ent:SetEngineActive( false ) end
+	if Ent.GetAI then Ent:SetAI( false ) end
 
 	-- allow rebuild of passenger seats
 	Ent.pPodKeyIndex = nil
