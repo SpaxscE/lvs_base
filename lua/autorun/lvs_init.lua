49027eda98d14d12cba36c7080dfb38d6bcd3a63
Luna <swagnemite@gmx.de>
2023-10-18 22:20:34 +0200

    fix more issues with duping

--- lua/autorun/lvs_init.lua
+++ lua/autorun/lvs_init.lua
@@ -1,7 +1,7 @@
 
 LVS = istable( LVS ) and LVS or {}
 
-LVS.VERSION = 174
+LVS.VERSION = 175
 LVS.VERSION_GITHUB = 0
 LVS.VERSION_TYPE = ".GIT"
 
--- lua/entities/lvs_base/sv_duping.lua
+++ lua/entities/lvs_base/sv_duping.lua
@@ -2,23 +2,58 @@
 -- attempt at fixing dupe support
 
 function ENT:PostEntityPaste(Player,Ent,CreatedEntities)
-	if Ent.SetlvsReady then Ent:SetlvsReady( false ) end
-	if Ent.GetActive then Ent:SetActive( false ) end
-	if Ent.GetEngineActive then Ent:SetEngineActive( false ) end
-	if Ent.GetAI then Ent:SetAI( false ) end
-
-	-- fix issue with door handlers
-	Ent._DoorHandlers = nil
-
-	-- allow rebuild of passenger seats
-	Ent.pPodKeyIndex = nil
-	Ent.pSeats = nil
-
-	-- allow rebuild of crosshair trace filter
-	Ent.CrosshairFilterEnts  = nil
+	if self.SetAI then self:SetAI( false ) end
+	if self.SetActive then self:SetActive( false ) end
+	if self.SetEngineActive then self:SetEngineActive( false ) end
 end
 
---[[
+local Active
+local EngineActive
+local CrosshairFilterEnts
+local DoorHandlers
+local pPodKeyIndex
+local pSeats
+
 function ENT:PreEntityCopy()
+	Active = self:GetActive()
+	EngineActive = self:GetEngineActive()
+
+	self:SetActive( false )
+	self:SetEngineActive( false )
+
+
+	self:SetlvsReady( false )
+
+
+	CrosshairFilterEnts = self.CrosshairFilterEnts
+	DoorHandlers = self._DoorHandlers
+	pSeats = self.pSeats
+	pPodKeyIndex = self.pPodKeyIndex
+
+	self.CrosshairFilterEnts = nil
+	self._DoorHandlers = nil
+	self.pPodKeyIndex = nil
+	self.pSeats = nil
 end
-]]
+
+function ENT:PostEntityCopy()
+	self:SetActive( Active )
+	self:SetEngineActive( EngineActive )
+
+	Active = nil
+	EngineActive = nil
+
+
+	self:SetlvsReady( true )
+
+
+	self.CrosshairFilterEnts = CrosshairFilterEnts
+	self._DoorHandlers = DoorHandlers
+	self.pPodKeyIndex = pPodKeyIndex
+	self.pSeats = pSeats
+
+	CrosshairFilterEnts = nil
+	DoorHandlers = nil
+	pSeats = nil
+	pPodKeyIndex = nil
+end
