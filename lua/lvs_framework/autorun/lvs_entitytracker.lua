f9db63e159212a0c4b706b4aa33265395e20b66e
Luna <swagnemite@gmx.de>
2023-10-29 20:00:33 +0100

    rework entity relationship rules

--- lua/lvs_framework/autorun/lvs_entitytracker.lua
+++ lua/lvs_framework/autorun/lvs_entitytracker.lua
@@ -22,45 +22,56 @@ function LVS:GetVehicles()
 end
 
 local Teams = {
-	["npc_breen"] = 1,
-	["npc_combine_s"] = 1,
-	["npc_combinedropship"] = 1,
-	["npc_combinegunship"] = 1,
-	["npc_crabsynth"] = 1,
-	["npc_cscanner"] = 1,
-	["npc_helicopter"] = 1,
-	["npc_manhack"] = 1,
-	["npc_metropolice"] = 1,
-	["npc_mortarsynth"] = 1,
-	["npc_sniper"] = 1,
-	["npc_stalker"] = 1,
-	["npc_strider"] = 1,
-	["npc_hunter"] = 1,
-	["monster_human_grunt"] = 1,
-	["monster_human_assassin"] = 1,
-	["monster_sentry"] = 1,
+	["npc_kleiner"] = 0,
+	["npc_monk"] = 0,
+	["npc_mossman"] = 0,
+	["npc_vortigaunt"] = 0,
+	["npc_alyx"] = 0,
+	["npc_barney"] = 0,
+	["npc_citizen"] = 0,
+	["npc_dog"] = 0,
+	["npc_eli"] = 0,
 
-	["npc_kleiner"] = 2,
-	["npc_monk"] = 2,
-	["npc_mossman"] = 2,
-	["npc_vortigaunt"] = 2,
-	["npc_alyx"] = 2,
-	["npc_barney"] = 2,
-	["npc_citizen"] = 2,
-	["npc_dog"] = 2,
-	["npc_eli"] = 2,
-	["monster_scientist"] = 2,
-	["monster_barney"] = 2,
-
-	["npc_fastzombie"] = 3,
+	["npc_breen"] = 3,
+	["npc_combine_s"] = 3,
+	["npc_combinedropship"] = 3,
+	["npc_combinegunship"] = 3,
+	["npc_crabsynth"] = 3,
+	["npc_cscanner"] = 3,
+	["npc_clawscanner"] = 3,
+	["npc_helicopter"] = 3,
+	["npc_manhack"] = 3,
+	["npc_metropolice"] = 3,
+	["npc_mortarsynth"] = 3,
+	["npc_sniper"] = 3,
+	["npc_stalker"] = 3,
+	["npc_strider"] = 3,
+	["npc_hunter"] = 3,
 	["npc_headcrab"] = 3,
 	["npc_headcrab_black"] = 3,
 	["npc_headcrab_fast"] = 3,
 	["npc_antlion"] = 3,
 	["npc_antlionguard"] = 3,
+	["npc_antlion_worker"] = 3,
+	["npc_zombine"] = 3,
 	["npc_zombie"] = 3,
 	["npc_zombie_torso"] = 3,
 	["npc_poisonzombie"] = 3,
+	["npc_fastzombie"] = 3,
+	["npc_fastzombie_torso"] = 3,
+	["npc_portal_turret_floor"] = 3,
+	["npc_rocket_turret"] = 3,
+	["npc_rollermine"] = 3,
+	["npc_turret_floor"] = 3,
+	["npc_turret_ceiling"] = 3,
+
+	["monster_scientist"] = 0,
+	["monster_barney"] = 0,
+
+	["monster_human_grunt"] = 3,
+	["monster_human_assassin"] = 3,
+	["monster_sentry"] = 3,
+	["monster_alien_controller"] = 3,
 	["monster_alien_grunt"] = 3,
 	["monster_alien_slave"] = 3,
 	["monster_gargantua"] = 3,
@@ -72,9 +83,30 @@ local Teams = {
 	["monster_nihilanth"] = 3,
 	["monster_bigmomma"] = 3,
 	["monster_babycrab"] = 3,
+	["monster_turret"] = 3,
+	["monster_sentry"] = 3,
 }
+
 function LVS:GetNPCRelationship( npc_class )
 	return Teams[ npc_class ] or 0
+end
+
+function LVS:SetNPCRelationship( npc )
+	local Team = LVS:GetNPCRelationship( npc:GetClass() )
+
+	for _, veh in pairs( LVS:GetVehicles() ) do
+		if not veh:IsEnemy( npc ) then continue end
+
+		npc:AddEntityRelationship( veh, D_HT, 50 )
+	end
+end
+
+function LVS:SetVehicleRelationship( veh )
+	for _, npc in ipairs( ents.GetAll() ) do
+		if not isfunction( npc.IsNPC ) or not npc:IsNPC() or not veh:IsEnemy( npc ) then continue end
+
+		npc:AddEntityRelationship( veh, D_HT, 50 )
+	end
 end
 
 hook.Add( "OnEntityCreated", "!!!!lvsEntitySorter", function( ent )
@@ -83,6 +115,10 @@ hook.Add( "OnEntityCreated", "!!!!lvsEntitySorter", function( ent )
 
 		if isfunction( ent.IsNPC ) and ent:IsNPC() then
 			table.insert( LVS.NPCsStored, ent )
+
+			if SERVER then
+				LVS:SetNPCRelationship( ent )
+			end
 		end
 
 		if ent.LVS then 
@@ -91,6 +127,10 @@ hook.Add( "OnEntityCreated", "!!!!lvsEntitySorter", function( ent )
 			end
 
 			table.insert( LVS.VehiclesStored, ent )
+
+			if SERVER then
+				LVS:SetVehicleRelationship( ent )
+			end
 		end
 
 		if ent.LFS then 
