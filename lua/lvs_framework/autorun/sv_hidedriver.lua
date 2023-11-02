4a61396ff115a3c505d95f153940ff8ced50e51c
Luna <swagnemite@gmx.de>
2023-11-02 16:13:23 +0100

    added pac support for hidedriver

--- lua/lvs_framework/autorun/sv_hidedriver.lua
+++ lua/lvs_framework/autorun/sv_hidedriver.lua
@@ -3,10 +3,14 @@ hook.Add( "PlayerEnteredVehicle", "!!LVS_HideDriver", function( ply, Pod )
 	if not Pod.HidePlayer then return end
 
 	ply:SetNoDraw( true )
+
+	if pac then pac.TogglePartDrawing( ply, 0 ) end
 end )
 
 hook.Add( "PlayerLeaveVehicle", "!!LVS_HideDriver", function( ply, Pod )
 	if not Pod.HidePlayer then return end
 
 	ply:SetNoDraw( false )
+
+	if pac then pac.TogglePartDrawing( ply, 1 ) end
 end )
