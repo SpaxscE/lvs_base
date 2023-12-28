f777a769dad700eaee2738f0c04f981b58c542c3
Luna <swagnemite@gmx.de>
2023-12-28 10:01:35 +0100

    fix view not aligning properly

--- lua/entities/lvs_base/sv_pod.lua
+++ lua/entities/lvs_base/sv_pod.lua
@@ -66,6 +66,7 @@ function ENT:SetPassenger( ply )
 
 	if IsValid( DriverSeat ) and not IsValid( DriverSeat:GetDriver() ) and not ply:KeyDown( IN_WALK ) and not AI and AllowedToBeDriver then
 		ply:EnterVehicle( DriverSeat )
+		self:AlignView( ply )
 	else
 		local Seat = NULL
 		local Dist = 500000
@@ -84,11 +85,13 @@ function ENT:SetPassenger( ply )
 
 		if IsValid( Seat ) then
 			ply:EnterVehicle( Seat )
+			self:AlignView( ply, true )
 		else
 			if IsValid( DriverSeat ) then
 				if not IsValid( self:GetDriver() ) and not AI then
 					if AllowedToBeDriver then
 						ply:EnterVehicle( DriverSeat )
+						self:AlignView( ply )
 					else
 						hook.Run( "LVS.OnPlayerCannotDrive", ply, self )
 					end
