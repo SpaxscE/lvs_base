412397c87a93223fd700d990d61f4fb12d8ccde0
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-08-11 14:06:30 +0200

    people still believe this is an issue. Insane.

--- lua/entities/lvs_base_doorhandler.lua
+++ lua/entities/lvs_base_doorhandler.lua
@@ -53,6 +53,16 @@ function ENT:IsOpen()
 	return self:GetActive()
 end
 
+function ENT:InRange( ply, Range )
+	local boxOrigin = self:GetPos()
+	local boxAngles = self:GetAngles()
+	local boxMins = self:GetMins()
+	local boxMaxs = self:GetMaxs()
+
+	local HitPos, _, _ = util.IntersectRayWithOBB( ply:GetShootPos(), ply:GetAimVector() * Range, boxOrigin, boxAngles, boxMins, boxMaxs )
+
+	return isvector( HitPos )
+end
 if SERVER then
 	AccessorFunc(ENT, "soundopen", "SoundOpen", FORCE_STRING)
 	AccessorFunc(ENT, "soundclose", "SoundClose", FORCE_STRING)
@@ -71,11 +81,13 @@ if SERVER then
 
 		local ent = net.ReadEntity()
 
-		if not IsValid( ent ) then return end
+		if not IsValid( ent ) or not ent._UseTargetAllowed or not ent.UseRange or ply:InVehicle() then return end
 
-		if (ply:GetPos() - ent:GetPos()):Length() > (ent.UseRange or 75) * 2 then return end
+		local Range = ent.UseRange * 2
 
-		if not ent._UseTargetAllowed or ply:InVehicle() then return end
+		if (ply:GetPos() - ent:GetPos()):Length() > Range then return end
+
+		if not ent:InRange( ply, Range ) then return end
 
 		ent:Use( ply, ply )
 	end)
@@ -309,18 +321,11 @@ function ENT:DrawTranslucent()
 
 	if not IsValid( ply ) or ply:InVehicle() or not ply:KeyDown( IN_SPEED ) then return end
 
-	local boxOrigin = self:GetPos()
-	local boxAngles = self:GetAngles()
-	local boxMins = self:GetMins()
-	local boxMaxs = self:GetMaxs()
-
-	local EntTable = self:GetTable()
-
-	local HitPos, _, _ = util.IntersectRayWithOBB( ply:GetShootPos(), ply:GetAimVector() * EntTable.UseRange, boxOrigin, boxAngles, boxMins, boxMaxs )
-
-	local InRange = isvector( HitPos )
+	local InRange = self:InRange( ply, self.UseRange )
 
 	if InRange then
+		local EntTable = self:GetTable()
+
 		local Use = ply:KeyDown( IN_USE )
 
 		if EntTable.old_Use ~= Use then
@@ -335,6 +340,13 @@ function ENT:DrawTranslucent()
 	end
 
 	if not LVS.DeveloperEnabled then return end
+
+	local boxOrigin = self:GetPos()
+	local boxAngles = self:GetAngles()
+	local boxMins = self:GetMins()
+	local boxMaxs = self:GetMaxs()
+
+	local EntTable = self:GetTable()
 
 	local Col = InRange and EntTable.ColorSelect or EntTable.ColorNormal
 
