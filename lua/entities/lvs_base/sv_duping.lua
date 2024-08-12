8279533df6b7ae4b900e326680aaa256c446bfce
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-08-12 12:29:17 +0200

    fix duping, alternative way
    https://github.com/Facepunch/garrysmod/pull/2100
    https://github.com/SpaxscE/lvs_base/issues/72

--- lua/entities/lvs_base/sv_duping.lua
+++ lua/entities/lvs_base/sv_duping.lua
@@ -2,6 +2,8 @@
 -- attempt at fixing dupe support
 
 function ENT:PostEntityPaste(Player,Ent,CreatedEntities)
+	self.MaxHealth = self:GetHP()
+
 	if not self.SetAI then return end
 	
 	if IsValid( Player ) and Player:IsAdmin() then return end
@@ -11,30 +13,17 @@ end
 
 local Active
 local EngineActive
-local CrosshairFilterEnts
-local DoorHandlers
-local pPodKeyIndex
-local pSeats
+local HP
 
 function ENT:PreEntityCopy()
 	Active = self:GetActive()
 	EngineActive = self:GetEngineActive()
+	HP = self:GetHP()
 
 	self:SetlvsReady( false )
 	self:SetActive( false )
 	self:SetEngineActive( false )
-
-	CrosshairFilterEnts = self.CrosshairFilterEnts
-	DoorHandlers = self._DoorHandlers
-	pSeats = self.pSeats
-	pPodKeyIndex = self.pPodKeyIndex
-
-	self.CrosshairFilterEnts = nil
-	self._DoorHandlers = nil
-	self.pPodKeyIndex = nil
-	self.pSeats = nil
-
-	return true
+	self:SetHP( self:GetMaxHP() )
 end
 
 function ENT:PostEntityCopy()
@@ -44,18 +33,33 @@ function ENT:PostEntityCopy()
 		self:SetlvsReady( true )
 		self:SetActive( Active )
 		self:SetEngineActive( EngineActive )
+		self:SetHP( HP )
 
 		Active = nil
 		EngineActive = nil
+		HP = nil
 	end)
+end
 
-	self.CrosshairFilterEnts = CrosshairFilterEnts
-	self._DoorHandlers = DoorHandlers
-	self.pPodKeyIndex = pPodKeyIndex
-	self.pSeats = pSeats
+function ENT:OnEntityCopyTableFinish( data )
+	data.CrosshairFilterEnts = nil
+	data._DoorHandlers = nil
+	data.pPodKeyIndex = nil
+	data.pSeats = nil
+	data.WEAPONS = nil
+	data._armorParts = nil
+	data._dmgParts = nil
+	data._DoorHandlers = nil
+	data._pdsParts = nil
 
-	CrosshairFilterEnts = nil
-	DoorHandlers = nil
-	pSeats = nil
-	pPodKeyIndex = nil
+	for id, entry in pairs( data ) do
+		if not isfunction( entry ) then continue end
+
+		data[ id ] = nil
+	end
+
+	-- stuff below is things like constraints or DeleteOnRemove still referencing the old object. These need to go
+	data.OnDieFunctions = nil
+	data.Constraints = nil
+	data._children = nil
 end
