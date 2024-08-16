7147137b9c7ac1d78b09483f5c1bc9dfb94213e5
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-08-16 19:03:56 +0200

    rework rewards

--- gamemodes/lvs/gamemode/sv_rewards.lua
+++ gamemodes/lvs/gamemode/sv_rewards.lua
@@ -1,13 +1,13 @@
 
-GM.MoneyPerKill = 25
-GM.MoneyPerKillVehicle = 100
+GM.MoneyPerKill = 20
+GM.MoneyPerKillVehicle = 500
 
 GM.MoneyPerTeamKill = 250
-GM.MoneyPerTeamKillVehicle = 500
+GM.MoneyPerTeamKillVehicle = 1000
 
-GM.MoneyPerFortificationKill = 150
-GM.MoneyPerFortificationTeamKill = 250
+GM.MoneyPerFortificationKill = 200
+GM.MoneyPerFortificationTeamKill = 200
 
 GM.MoneyPerGoal = 40
-GM.MoneyPerGoalDrop = 15
-GM.MoneyPerGoalDelivered = 400
+GM.MoneyPerGoalDrop = 80
+GM.MoneyPerGoalDelivered = 160
