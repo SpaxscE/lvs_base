4de32b767283d653546db1567ab7b451b7cc9c34
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-08-16 11:30:33 +0200

    buff fortification money reward to give attacking team an advantage

--- gamemodes/lvs/gamemode/sv_rewards.lua
+++ gamemodes/lvs/gamemode/sv_rewards.lua
@@ -1,12 +1,12 @@
 
-GM.MoneyPerKill = 50
+GM.MoneyPerKill = 25
 GM.MoneyPerKillVehicle = 100
 
 GM.MoneyPerTeamKill = 250
 GM.MoneyPerTeamKillVehicle = 500
 
-GM.MoneyPerFortificationKill = 25
-GM.MoneyPerFortificationTeamKill = 50
+GM.MoneyPerFortificationKill = 150
+GM.MoneyPerFortificationTeamKill = 250
 
 GM.MoneyPerGoal = 40
 GM.MoneyPerGoalDrop = 15
