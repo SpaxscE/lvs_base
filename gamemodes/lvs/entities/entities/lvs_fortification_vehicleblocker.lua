9051dc45e247a2039a7ca917f8dae7a2b4c69af2
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-08-17 18:16:25 +0200

    Update lvs_fortification_vehicleblocker.lua

--- gamemodes/lvs/entities/entities/lvs_fortification_vehicleblocker.lua
+++ gamemodes/lvs/entities/entities/lvs_fortification_vehicleblocker.lua
@@ -4,7 +4,7 @@ ENT.Base = "lvs_fortification"
 
 if CLIENT then return end
 
-ENT.DamageIgnoreType = DMG_SNIPER + DMG_BULLET + DMG_CLUB + DMG_DROWN + DMG_PARALYZE + DMG_NERVEGAS + DMG_POISON + DMG_BURN
+ENT.DamageIgnoreType = DMG_CLUB + DMG_DROWN + DMG_PARALYZE + DMG_NERVEGAS + DMG_POISON + DMG_BURN
 
 function ENT:PhysicsCollide( data, physobj )
 end
