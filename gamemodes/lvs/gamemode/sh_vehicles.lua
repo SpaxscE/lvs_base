142bddea8d95f7ded46b59fdad36cd909ca4cd17
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-08-18 10:00:31 +0200

    rework helicopter prices again

--- gamemodes/lvs/gamemode/sh_vehicles.lua
+++ gamemodes/lvs/gamemode/sh_vehicles.lua
@@ -117,7 +117,7 @@ function GM:BuildVehiclePrices()
 
 				-- helis are just too strong
 				if v.t.Base == "lvs_base_helicopter" then
-					PriceClassPunishment = 400
+					PriceClassPunishment = 250 * (((v.t.TurnRatePitch or 1) + (v.t.TurnRateYaw or 1) + (v.t.TurnRateRoll or 1)) * (v.t.ThrustUp or 1))
 				end
 	
 				self.VehiclePrices[s] = math.Round( IgnoreForce * 0.1 +  DamageForce * 0.01 + (MaxHealth + MaxShield * 100) * 0.1 + MaxVelocity * 0.1, 0 ) + PriceClassPunishment
