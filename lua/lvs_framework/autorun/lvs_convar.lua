3e04bb9e13e26f9b0f0f3cd35c5907037fd683c6
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-09-01 10:10:28 +0200

    change default return delta to 2

--- lua/lvs_framework/autorun/lvs_convar.lua
+++ lua/lvs_framework/autorun/lvs_convar.lua
@@ -50,7 +50,7 @@ CreateClientConVar( "lvs_mouseaim_type", 0, true, false)
 CreateClientConVar( "lvs_edit_hud", 1, true, false)
 CreateClientConVar( "lvs_sensitivity_x", 1, true, true)
 CreateClientConVar( "lvs_sensitivity_y", 1, true, true)
-CreateClientConVar( "lvs_return_delta", 1, true, true)
+CreateClientConVar( "lvs_return_delta", 2, true, true)
 
 LVS.cvarCamFocus = CreateClientConVar( "lvs_camerafocus", 0, true, false)
 
