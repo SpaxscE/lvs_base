1342176e217e54145bb3c31fedc8173a3f3acd72
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-09-06 15:18:05 +0200

    Update init.lua

--- gamemodes/lvs/gamemode/gamelogic/init.lua
+++ gamemodes/lvs/gamemode/gamelogic/init.lua
@@ -4,6 +4,7 @@ GAMESTATE_BUILD = 1
 GAMESTATE_START = 2
 GAMESTATE_MAIN = 3
 GAMESTATE_END = 4
+GAMESTATE_STOP = 5
 
 function GM:GetGameProgression()
 	local ent = self:GameNetworkEntity()
@@ -40,7 +41,7 @@ end
 function GM:GetGameState()
 	local ent = self:GameNetworkEntity()
 
-	if not IsValid( ent ) then return GAMESTATE_WAIT_FOR_PLAYERS end
+	if not IsValid( ent ) then return GAMESTATE_STOP end
 
 	return ent:GetGameState()
 end
