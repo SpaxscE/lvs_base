b9842c1c670f1b256df4886936f97ac86ff65faa
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-12-15 20:53:33 +0100

    Update lvs_explosion_bomb.lua

--- lua/effects/lvs_explosion_bomb.lua
+++ lua/effects/lvs_explosion_bomb.lua
@@ -244,7 +244,7 @@ function EFFECT:Explosion( pos , scale )
 	if not IsValid( emitter ) then return end
 
 	for i = 0, 40 do
-		local particle = emitter:Add( "particles/flamelet"..math.random(1,5), pos )
+		local particle = emitter:Add( "effects/lvs_base/flamelet"..math.random(1,5), pos )
 
 		if particle then
 			particle:SetVelocity( VectorRand() * 1500 * scale )
