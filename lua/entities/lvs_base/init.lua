a30716bf21e140ee4f89e476bf6e81f76a4cf4d6
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-12-28 05:28:24 +0100

    incr version number

--- lua/entities/lvs_base/init.lua
+++ lua/entities/lvs_base/init.lua
@@ -57,7 +57,6 @@ function ENT:Initialize()
 	self:SetUseType( SIMPLE_USE )
 	self:SetRenderMode( RENDERMODE_NORMAL )
 	self:AddFlags( FL_OBJECT )
-	self:AddEFlags( EFL_NO_PHYSCANNON_INTERACTION )
 
 	local PObj = self:GetPhysicsObject()
 
