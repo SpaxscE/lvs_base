f0f59b00c23d66adf604b2d183c7f1b0045a9dee
SpaxscE <stephanklings@freenet.de>
2025-10-09 13:45:39 +0200

    Update lvs_wheeldrive_lighthandler.lua

--- lua/entities/lvs_wheeldrive_lighthandler.lua
+++ lua/entities/lvs_wheeldrive_lighthandler.lua
@@ -282,9 +282,7 @@ function ENT:ClearProjectedTextures()
 end
 
 local function DistanceMul( ent )
-	local ply = LocalPlayer()
-
-	local dist = (ply:GetPos() - ent:GetPos()):LengthSqr() / (ply:InVehicle() and 500000 or 10000000)
+	local dist = (LocalPlayer():GetPos() - ent:GetPos()):LengthSqr() / 500000
 
 	return math.max( 1 - dist, 0 )
 end
