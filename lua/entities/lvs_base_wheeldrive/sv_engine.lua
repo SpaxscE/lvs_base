3bf41fe175b0879081242b65ec33effa4c107cbc
SpaxscE <stephanklings@freenet.de>
2025-10-12 11:56:35 +0200

    Update sv_engine.lua

--- lua/entities/lvs_base_wheeldrive/sv_engine.lua
+++ lua/entities/lvs_base_wheeldrive/sv_engine.lua
@@ -76,7 +76,7 @@ function ENT:GetEngineTorque()
 		if Gear < NumGears and Ratio < 0.5 then
 			local engine = self:GetEngine()
 
-			if IsValid( engine ) then
+			if IsValid( engine ) and Ratio < 0.5 * self:GetThrottle() then
 				local dmginfo = DamageInfo()
 				dmginfo:SetDamage( 1 )
 				dmginfo:SetAttacker( self )
