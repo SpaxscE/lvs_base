a276643b893520e50c35b7b696b4b653384161a6
SpaxscE <stephanklings@freenet.de>
2025-10-14 10:32:22 +0200

    fix defaults

--- gamemodes/sandbox/entities/weapons/gmod_tool/stools/lvscarwheelchanger.lua
+++ gamemodes/sandbox/entities/weapons/gmod_tool/stools/lvscarwheelchanger.lua
@@ -3,7 +3,7 @@
 TOOL.Category		= "LVS"
 TOOL.Name			= "#Wheel Editor"
 
-TOOL.ClientConVar[ "model" ] = "models/diggercars/kubel/kubelwagen_wheel.mdl"
+TOOL.ClientConVar[ "model" ] = "models/props_vehicles/carparts_wheel01a.mdl"
 TOOL.ClientConVar[ "camber" ] = 0
 TOOL.ClientConVar[ "caster" ] = 0
 TOOL.ClientConVar[ "toe" ] = 0
