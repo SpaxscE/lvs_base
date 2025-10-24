87bf621677d03edaf8643f26bd9164e1a28a6a30
SpaxscE <stephanklings@freenet.de>
2025-10-24 22:52:19 +0200

    Update lvs_physics_trackdust.lua

--- lua/effects/lvs_physics_trackdust.lua
+++ lua/effects/lvs_physics_trackdust.lua
@@ -1,5 +1,5 @@
 
-local DustMat = {
+EFFECT.DustMat = {
 	"effects/lvs/track_debris_01",
 	"effects/lvs/track_debris_02",
 }
@@ -13,37 +13,13 @@ function EFFECT:Init( data )
 
 	local dir = data:GetNormal()
 
-	local start = ent:GetPos()
-	local emitter = ent:GetParticleEmitter( start )
-	local emitter3D = ent:GetParticleEmitter3D( start )
+	local emitter = ent:GetParticleEmitter( ent:GetPos() )
 
 	local VecCol = (render.GetLightColor( pos + dir ) * 0.5 + Vector(0.5,0.4,0.3)) * 255
 
-	local traceData = {
-		start = pos + Vector(0,0,1),
-		endpos = pos - Vector(0,0,1),
-		mask = MASK_SOLID_BRUSHONLY,
-	}
-
-	local trace = util.TraceLine( traceData )
-
-	if trace.Hit then
-		local Ang = trace.HitNormal:Angle()
-		Ang:RotateAroundAxis( trace.HitNormal, math.Rand(-180,180) )
-
-		local pHit = emitter3D:Add( DustMat[ math.random(1,#DustMat) ], trace.HitPos + trace.HitNormal )
-		pHit:SetStartSize( 15 )
-		pHit:SetEndSize( 15 )
-		pHit:SetDieTime( math.Rand(0.75,1) )
-		pHit:SetStartAlpha( 255 )
-		pHit:SetEndAlpha( 0 )
-		pHit:SetColor( math.min( VecCol.r, 255 ), math.min( VecCol.g, 255 ), math.min( VecCol.b, 255 ) )
-		pHit:SetAngles( Ang )
-	end
-
 	local scale = math.Clamp( size / 23, 0.5, 1.25 )
 
-	local particle = emitter:Add( DustMat[ math.random(1,#DustMat) ], pos + dir * 5 * scale + VectorRand() * 5 * scale )
+	local particle = emitter:Add( self.DustMat[ math.random(1,#self.DustMat) ] , pos + dir * 5 * scale + VectorRand() * 5 * scale )
 
 	if not particle then return end
 
@@ -51,8 +27,8 @@ function EFFECT:Init( data )
 	particle:SetDieTime( math.Rand(0.4,0.6) )
 	particle:SetAirResistance( 10 ) 
 	particle:SetStartAlpha( math.random(100,255) )
-	particle:SetStartSize( 8 * scale )
-	particle:SetEndSize( math.random(30,40) * scale )
+	particle:SetStartSize( 6 * scale )
+	particle:SetEndSize( math.random(20,25) * scale )
 	particle:SetRollDelta( math.Rand(-1,1) )
 	particle:SetColor( math.min( VecCol.r, 255 ), math.min( VecCol.g, 255 ), math.min( VecCol.b, 255 ) )
 	particle:SetGravity( Vector(0,0,-600) )
